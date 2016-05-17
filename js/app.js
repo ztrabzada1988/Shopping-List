e4e7a30ec1ea5b3891eddec27cc6244feca76096
Zubair Trabzada <zubair.trabzada@gmail.com>
2016-05-17 00:21:20 -0700

    Formatting Update

--- js/app.js
+++ js/app.js
@@ -1,31 +1,43 @@
 // step 1:  Defining javascript functions
 // function to add item
 function addItem() {
-    alert("I've just activated the addItem() function");
+    //alert("I've just activated the addItem() function");
 
-    var itemValue = $('.input').val();
+    var itemValue = $('.add-item').val();
 
-    alert(itemValue);
+    //alert(itemValue);
 
+    if (itemValue.length === 0) {
+        alert('Please add something!!');
+    }
 
+    //if the input is valid
+    else {
+        var row = $('<li><button class="checkbox-button">✓</button><span class="list-checkbox">' + itemValue + '</span><button class="crossbox-button">x</button></li>');
+    }
 
+    //add each row to previous
+    $('.buy-list').append(row);
+
+    //empty the input box after submit by resetting the value
+    $('.add-item').val('');
 
 }
 // function to check one item (already purchased)
 function checkItem() {
-    alert("I've just activated the checkItem() function");
+    // alert("I've just activated the checkItem() function"); //
     $(this).parent().toggleClass('checkbox');
 }
 
 // function to delete one item
 function deleteItem() {
-    alert("I've just activated the deleteItem() function");
+    // alert("I've just activated the deleteItem() function"); //
     $(this).parent().remove();
 }
 
 // function to delete all the items (clear list)
 function delleteAll() {
-    alert("I've just activated the delleteAll() function");
+    // alert("I've just activated the delleteAll() function"); //
     $('.buy-list').empty();
 }
 
