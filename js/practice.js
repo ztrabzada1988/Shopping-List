2b53b572fe3b559ebc9a25715290cdcd0643eb45
Zubair Trabzada <zubair.trabzada@gmail.com>
2016-12-10 16:27:22 -0800

    practice js

--- js/practice.js
+++ js/practice.js
@@ -18,7 +18,7 @@ function addItem() {
 
 // function to delete items from the list
 function deleteItem() {
-    $(this).remove();
+    $(this).parent().remove();
 }
 
 // function to check items off from the list
